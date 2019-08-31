2828bc673e155f87d27e1e9f70519451347d9ee6
ComeYummy <naoki.kameyama@gmail.com>
2019-08-31 10:16:35 +0900

    git init

---
+++ Day14-1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Day14-1
+//
+//  Created by Naoki Kameyama on 2019/08/31.
+//  Copyright © 2019 Naoki Kameyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Day14-1Tests/Day14_1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Day14_1Tests.swift
+//  Day14-1Tests
+//
+//  Created by Naoki Kameyama on 2019/08/31.
+//  Copyright © 2019 Naoki Kameyama. All rights reserved.
+//
+
+import XCTest
+@testable import Day14_1
+
+class Day14_1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
